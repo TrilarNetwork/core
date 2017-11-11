feed41e07728b76e7055db5248e385e1bbc71318
Samie <Samie@iMelvin>
2017-11-11 01:27:57 +0100

    fixes

--- Trilar_McCore/src/me/shizleshizle/core/commands/Help.java
+++ Trilar_McCore/src/me/shizleshizle/core/commands/Help.java
@@ -13,7 +13,7 @@ import me.shizleshizle.core.utils.ErrorMessages;
 import me.shizleshizle.core.utils.ErrorMessages.Messages;
 
 public class Help implements CommandExecutor {
- 	public static String prefix = ChatColor.YELLOW.toString() + ChatColor.BOLD + "help" + ChatColor.GOLD + " >> " + ChatColor.YELLOW;
+ 	public static String prefix = ChatColor.YELLOW.toString() + ChatColor.BOLD + "Help" + ChatColor.GOLD + " >> " + ChatColor.YELLOW;
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("help")) {
