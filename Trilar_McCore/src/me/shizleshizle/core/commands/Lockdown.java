e8d22c36723ca61dbc114a6395dba0e93c2a905c
shizleshizle <Dani.raj25@gmail.com>
2017-11-11 01:38:30 +0100

    fixes

--- Trilar_McCore/src/me/shizleshizle/core/commands/Lockdown.java
+++ Trilar_McCore/src/me/shizleshizle/core/commands/Lockdown.java
@@ -6,16 +6,19 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitScheduler;
 
 import me.shizleshizle.core.Main;
 import me.shizleshizle.core.objects.User;
 import me.shizleshizle.core.permissions.Perm;
 import me.shizleshizle.core.permissions.PermGroup;
+import me.shizleshizle.core.utils.AutoB;
 import me.shizleshizle.core.utils.ErrorMessages;
 import me.shizleshizle.core.utils.ErrorMessages.Messages;
 
 public class Lockdown implements CommandExecutor {
  	public static String prefix = ChatColor.YELLOW.toString() + ChatColor.BOLD + "lockdown" + ChatColor.GOLD + " >> " + ChatColor.YELLOW;
+ 	private static BukkitScheduler bs = Bukkit.getScheduler();
  	
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -29,7 +32,7 @@ public class Lockdown implements CommandExecutor {
 							if (!hasLockdown()) {
 								Main.lockdown = true;
 								Bukkit.broadcastMessage(ChatColor.RED + "Server is going in Lockdown mode in one minute!");
-								Bukkit.getScheduler().scheduleSyncDelayedTask(Main.p, () -> {
+								bs.scheduleSyncDelayedTask(Main.p, () -> {
 									if (hasLockdown()) {
 										Bukkit.broadcastMessage(ChatColor.RED.toString() + ChatColor.BOLD + "Server is going in Lock mode!");
 										initiateLockdown();
@@ -82,5 +85,12 @@ public class Lockdown implements CommandExecutor {
 	
 	public static boolean hasLockdown() {
 		return Main.lockdown;
+	}
+	
+	public static void cancel() {
+		boolean b = false;
+		b = AutoB.isBroadcasting();
+		bs.cancelTasks(Main.p);
+		if (b) AutoB.enable();
 	}
 }
