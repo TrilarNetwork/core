298f79da6f2fc5b9abb55025c9a418558928aa9b
Samie <Samie@iMelvin>
2018-01-08 19:49:14 +0100

    fixes

--- Trilar_McCore/src/me/shizleshizle/core/commands/CommandSpy.java
+++ Trilar_McCore/src/me/shizleshizle/core/commands/CommandSpy.java
@@ -56,7 +56,7 @@ public class CommandSpy implements CommandExecutor {
 				} else {
 					ErrorMessages.doErrorMessage(p, Messages.NOPERM, "/commandspy");
 				}
-			}
+			} 
 		}
 		return false;
 	}
