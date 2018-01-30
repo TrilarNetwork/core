195e79c786bf809e4a0cbc7ef9cd1d10061b46d6
Melvin <Melvin@192.168.178.213>
2018-01-30 13:52:19 +0100

    fixes

--- Trilar_McCore/src/me/shizleshizle/core/listeners/PlayerPreProcess.java
+++ Trilar_McCore/src/me/shizleshizle/core/listeners/PlayerPreProcess.java
@@ -1,14 +1,12 @@
 package me.shizleshizle.core.listeners;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 import me.shizleshizle.core.commands.CommandSpy;
-import me.shizleshizle.core.objects.User;
 
 public class PlayerPreProcess implements Listener {
 
